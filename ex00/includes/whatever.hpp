36df97248d6977f643f1d0fc22c7b39f95e9fce3
Woojeong <alsxmakt123@naver.com>
2026-05-20 16:34:19 +0200

    ex00 fait

---
+++ ex00/includes/whatever.hpp
@@ -0,0 +1,35 @@
+#ifndef WHATEVER_HPP
+# define	WHATEVER_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+
+template	<typename T>
+void	swap(T& a, T& b)
+{
+	T	temp = a;
+
+	a = b;
+	b = temp;
+}
+
+template	<typename T>
+T const&	min(T const& a, T const& b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+template	<typename T>
+T const&	max(T const& a, T const& b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
+#endif
